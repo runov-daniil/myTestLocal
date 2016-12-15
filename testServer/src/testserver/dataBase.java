03b7a0acd422bb814d0a1cc7b9e7a578435aa480
runov-daniil <runov-daniil@yandex.ru>
2016-12-15 21:53:47 +0300

    начало алгоритма авторизации

--- testServer/src/testserver/dataBase.java
+++ testServer/src/testserver/dataBase.java
@@ -23,7 +23,16 @@ public class dataBase {
     public static String autorization(String login, String password) throws ClassNotFoundException, SQLException {
         String authStatus = "0";
         Connection();
-        
+        rs = st.executeQuery("SELECT * FROM users;");
+        boolean flag = false;
+        while(rs.next()){
+            String getLogin = rs.getString("login");
+            String getPassword = rs.getString("password");
+            if((getLogin == login)&&(getPassword == password)){
+                flag = true;
+                break;
+            }
+        }
         closeConnection();
         return authStatus;
     }
