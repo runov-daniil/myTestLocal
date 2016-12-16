cd57d630d3eea1cdec29eeeac5a833c692b51c96
runov-daniil <runov-daniil@yandex.ru>
2016-12-16 13:36:27 +0300

    Рефакторинг кода

--- testServer/src/testserver/dataBase.java
+++ testServer/src/testserver/dataBase.java
@@ -63,55 +63,41 @@ public class dataBase {
                 String fio = rsT.getString("fio");
                 element.add(id);element.add(login);
                 element.add(fio);
-                System.out.println(id + " " + " " + login + " " + fio);
                 table.add(element);
-                System.out.println("Учитель добавлен на форму");
             }
         }
-        System.out.println("Формирую заголовки таблицы");
         Vector header = new Vector();
         header.add("id_user");header.add("Логин");header.add("ФИО");
         DefaultTableModel dtm = (DefaultTableModel)mainFrame.teacherTable.getModel();
         dtm.setDataVector(table, header);
         closeConnection();
-        System.out.println("Закончил обновление учителей");
     }
     //Обновление данных учеников
     public static void refreshStudents() throws ClassNotFoundException, SQLException {
         Connection();
-        System.out.println("Формирую списки учеников");
         rs = st.executeQuery("SELECT * FROM users;");
         Vector table = new Vector();
         while(rs.next()){
-            System.out.println("Перебираю списки");
             Vector element = new Vector();
             int id = rs.getInt("id_user");
             String login = rs.getString("login");
             String level = rs.getString("level");
             if(level.equals("student")){
-                System.out.println("Нашел ученика");
                 Statement stT = conn.createStatement();
                 ResultSet rsT = stT.executeQuery("SELECT fio FROM students WHERE id_user = '" + id + "';");
-                System.out.println("Нашел ФИО ученика");
                 String fio = rsT.getString("fio");
-                System.out.println("Сохранил фио");
                 Statement idClassSt = conn.createStatement();
                 ResultSet idClassRs = idClassSt.executeQuery("SELECT id_class FROM students WHERE id_user = '" + id + "';");
                 int Class = idClassRs.getInt("id_class");
-                System.out.println("Нашел ID класса");
                 Statement stNum = conn.createStatement();
                 ResultSet numClass = stNum.executeQuery("SELECT number FROM classes WHERE id_class = '" + Class + "';");
-                System.out.println("Определяю номер класса");
                 Statement stLit = conn.createStatement();
-                System.out.println("создаю новую стату");
                 ResultSet litera = stLit.executeQuery("SELECT litera FROM classes WHERE id_class = '" + Class + "';");
-                System.out.println("Определяю литеру");
                 String number = numClass.getString("number");
                 String thisLitera = litera.getString("litera");
                 number = number + thisLitera;
                 element.add(id);element.add(login);element.add(number);
                 element.add(fio);
-                System.out.println(id + " " + " " + login + " " + number + " " + fio);
                 table.add(element);
             }
         }
