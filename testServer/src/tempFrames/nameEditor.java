90f5b9fc89d80c8c9a36ddc188425a26f0f4c8a1
runov-daniil <runov-daniil@yandex.ru>
2017-01-05 18:53:41 +0300

    Добавлена форма редактирования имени предмета

---
+++ testServer/src/tempFrames/nameEditor.java
@@ -0,0 +1,44 @@
+package tempFrames;
+
+public class nameEditor extends javax.swing.JFrame {
+    private static nameEditor nameEditor = new nameEditor();
+    public nameEditor() {
+        initComponents();
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        frames.predmetsFrame.setEnable(true);
+    }//GEN-LAST:event_formWindowClosing
+    public static void main(boolean visible) {
+        nameEditor.setResizable(false);
+        nameEditor.setVisible(visible);
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // End of variables declaration//GEN-END:variables
+}
